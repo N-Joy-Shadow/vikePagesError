d4cdbaae20d8d987a6431ecb86bdc9e1178a0c2e
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-03-13 03:01:48 +0900

    commit init

---
+++ src/Pages/+config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'vike/types'
+import vikeReact from 'vike-react/config'
+
+export default {
+    title : "title",
+    ssr : true,
+    extends : vikeReact,
+} satisfies Config
